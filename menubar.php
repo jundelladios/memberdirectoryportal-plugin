d13866fb6ed38611114a52a6a570bfed309154a7
jundellagbo <jundellagbo10dd@gmail.com>
2024-10-23 09:51:57 +0800

    menu bar updates api

--- menubar.php
+++ menubar.php
@@ -2,14 +2,14 @@
 
 add_action( 'admin_bar_menu', 'memberdirectoryportal_adminbar', 999 );
 function memberdirectoryportal_adminbar( $wp ) {
-    $request = wp_remote_get( memberdirectoryportal_get_portalmain() . "/api/trpc/connector.connectedIntegrations", array(
+    $request = wp_remote_get( memberdirectoryportal_get_portalmain() . "/api/wp", array(
       'headers' => array(
         "X-REQUEST-HOMEURL-HOST" => home_url()
       )
     ) );
     $data = wp_remote_retrieve_body($request);
     $response = json_decode($data);
-    $url = isset($response->result->data->json->redirect) ? $response->result->data->json->redirect : null;
+    $url = isset($response->redirect) ? $response->redirect : null;
     $title = "MDP: ";
     $title .= $url ? "Connected" : "Not Connected";
     $args = array(
@@ -19,8 +19,8 @@ function memberdirectoryportal_adminbar( $wp ) {
       'meta'  => array( 'class' => 'member-directory-portal', 'target' => '_blank' )
   );
 
-  if(isset($response->result->data->json->integration->group->location_id)) {
-    update_option( 'mdp_location_id', $response->result->data->json->integration->group->location_id );
+  if(isset($response->integration->group->location_id)) {
+    update_option( 'mdp_location_id', $response->integration->group->location_id );
   }
 
   $wp->add_node( $args );
